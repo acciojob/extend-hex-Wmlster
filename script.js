0f54313782eeceb4acca05eed2c45d32768559a4
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-16 18:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+
+	let s="#";
+
+	for(let i=0;i<shortHex.length;i++)
+		{
+			s+=shortHex[i]+shortHex[i];
+		}
+
+	return s;
 };
 
 // Do not change the code below.
